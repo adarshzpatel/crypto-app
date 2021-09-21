d696688a7ff46658a4796e61cdd978307dd9587f
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2021-09-21 11:49:16 +0530

    setup redux store

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "69049f0243mshee4045759391c7cp1d41b1jsn0c54c10e6469",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest('/exchanges')
+    })
+  }),
+});
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
+//     headers: {
+//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//       'x-rapidapi-key': '69049f0243mshee4045759391c7cp1d41b1jsn0c54c10e6469'
+//     }
+//   };
